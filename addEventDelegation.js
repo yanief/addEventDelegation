429e1eeb14b92e2d9e3a20caf7d1d61995a2b9e4
Yanief Latief <pembalas.dendam@gmail.com>
2017-07-19 22:04:57 +0700

    Create addEventDelegation.js

---
+++ addEventDelegation.js
@@ -0,0 +1,54 @@
+(function(){
+  // heavily inspired by jQuery .on method
+
+  // now we can add multiple space separated event names with addEventListener
+  Element.prototype.addEventListener = (function(){
+    var addEventListener = Element.prototype.addEventListener;
+    return function(){
+      var args = arguments;
+      var eventNames = args[0];
+      var context = this;
+
+      eventNames
+      .split(' ')
+      .filter(Boolean)
+      .foreach(function(eventName){
+        var newArgs = [].slice.call(args);
+        newArgs[0] = eventName;
+        addEventListener.apply(context, newArgs);
+      });
+    };
+  }());
+
+  if (!Element.prototype.matches) 
+  Element.prototype.matches = 
+    Element.prototype.matchesSelector || 
+    Element.prototype.mozMatchesSelector ||
+    Element.prototype.msMatchesSelector || 
+    Element.prototype.oMatchesSelector || 
+    Element.prototype.webkitMatchesSelector ||
+    function(selector) {
+      var doc = this.document || this.ownerDocument;
+      var matches = doc.querySelectorAll(selector);
+      var i = matches.length;
+      while (--i >= 0 && matches.item(i) !== this) {}
+      return i > -1;            
+    };
+
+  // delegate events for bubbling events
+  Element.prototype.addEventDelegation = function(eventName, childSelector, callback){
+    if (typeof eventName !== 'string' || 
+      typeof childSelector !== 'string' || 
+      typeof callback !== 'function')
+      return;
+
+    var parent = this;
+    parent.addEventListener(eventName, function(event) {
+      var eventTarget = event.target;
+      var isAMatch = eventTarget.matches(childSelector);
+      if (!isAMatch) return;
+
+      callback.apply(eventTarget, arguments);
+    });
+  };  
+}());
